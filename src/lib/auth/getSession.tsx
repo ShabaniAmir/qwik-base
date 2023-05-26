ac5fe70dea2e91da6fb8ed825fe5e0a02cd4d39b
Amir Shabani <shabani.a.amir@gmail.com>
2023-05-26 16:21:46 -0400

    Cleanup

--- src/lib/auth/getSession.tsx
+++ src/lib/auth/getSession.tsx
@@ -19,6 +19,4 @@ export default server$(async (event: RequestEventLoader) => {
         });
         return null;
     }
-
-
 })
--- src/lib/auth/useSignout.tsx
+++ src/lib/auth/useSignout.tsx
@@ -1,9 +1,5 @@
 import { globalAction$, zod$ } from "@builder.io/qwik-city";
-import { z } from "zod";
-import prisma from "~/util/db";
-import bcrypt from "bcrypt";
-import Jwt from "jsonwebtoken";
-import { Session } from "./getSession";
+
 export default globalAction$(async (data, event) => {
     event.cookie.delete("token", {
         path: "/",
